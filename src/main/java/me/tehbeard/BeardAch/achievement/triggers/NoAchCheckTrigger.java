8bc8558c16db014b94aac7dd3fad854b59bb91e6
James <james@BeardNet.(none)>
2012-01-05 17:05:20 +0000

    Added noAch Trigger

---
+++ src/main/java/me/tehbeard/BeardAch/achievement/triggers/NoAchCheckTrigger.java
@@ -0,0 +1,46 @@
+package me.tehbeard.BeardAch.achievement.triggers;
+
+
+import java.util.HashSet;
+import java.util.List;
+
+import me.tehbeard.BeardAch.achievement.Achievement;
+import me.tehbeard.BeardAch.achievement.AchievementManager;
+import me.tehbeard.BeardAch.achievement.AchievementPlayerLink;
+
+import org.bukkit.entity.Player;
+
+/**
+ * Checks if a player does not have an achievement
+ * @author James
+ *
+ */
+public class NoAchCheckTrigger extends Trigger {
+
+	String ach;
+
+	public static ITrigger getInstance(String config) {
+		NoAchCheckTrigger n =new NoAchCheckTrigger();
+			n.ach = config;
+			
+			
+		
+		return n;
+	}
+
+	@Override
+	public boolean checkAchievement(Player player) {
+		//if player has an acheivement, return false
+		List<AchievementPlayerLink> achs = AchievementManager.getAchievements(player.getName());
+		boolean result = true;
+		if(achs !=null){
+			
+			for(AchievementPlayerLink a: achs){
+				if(a.getSlug().equals(ach)){result=false;}
+			}
+			
+		}
+		return result;
+	}
+
+}
