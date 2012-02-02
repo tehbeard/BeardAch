f483c4a83661129b86ae4bf3603e4c4e1c072a4c
tehbeard <beardyjames@live.co.uk>
2012-02-02 17:40:40 +0000

    Adds fancy output (/ach-fancy) option to flat file

--- src/main/java/me/tehbeard/BeardAch/dataSource/YamlDataSource.java
+++ src/main/java/me/tehbeard/BeardAch/dataSource/YamlDataSource.java
@@ -1,6 +1,7 @@
 package me.tehbeard.BeardAch.dataSource;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.HashSet;
@@ -16,14 +17,49 @@ public class YamlDataSource extends AbstractDataSource {
 
 	YamlConfiguration db;
 	File dbFile;
+	File fancy;
 	public YamlDataSource(Plugin plugin){
 		dbFile = new File(plugin.getDataFolder(),"database.yml");
+		fancy = new File(plugin.getDataFolder(),"ach.html");
 		db = YamlConfiguration.loadConfiguration(dbFile);
 	}
-	
+
 	public void dumpFancy() {
-		// TODO Auto-generated method stub
-		
+
+		try {
+			FileWriter fwrite = new FileWriter(fancy);
+			fwrite.write("<html>\n" +
+					"<head>\n" +
+					"<title>Achievements</title>\n" +
+					"</head>\n" +
+					"<body>\n");
+			ConfigurationSection dbb = db.getConfigurationSection("database");
+			if(dbb==null){}
+			Set<String> players = dbb.getKeys(false);
+			for(String player : players){
+				Set<AchievementPlayerLink> achs = getPlayersAchievements(player);
+				if(achs.size() > 0){
+					fwrite.write("<h2>" +
+							player + 
+						    "</h2>\n");
+					fwrite.write("<table>\n" +
+							"<tr><th>Achievement</th><th>Date unlocked</th></tr\n");
+					for(AchievementPlayerLink link : achs){
+						fwrite.write("<tr>\n" +
+								"<td>" + link.getAch().getName() + "</td>\n" +
+								"<td>" + link.getDate() + "</td>\n" +
+								"</tr>\n");	
+					}
+					fwrite.write("</table>\n");
+				}
+			}
+
+			fwrite.write("</body></html>\n");
+			fwrite.close();
+		} catch (IOException e) {
+			System.out.println("I/O error occured while trying to write out to file");
+		}
+
 	}
 
 	@Override
@@ -50,11 +86,11 @@ public class YamlDataSource extends AbstractDataSource {
 
 	@Override
 	public void flush() {
-		
+
 		try {
 			db.save(dbFile);
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
 		}
 	}
