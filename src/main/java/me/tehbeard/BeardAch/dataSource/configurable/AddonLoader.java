cadcdf99237d32837d6bce50585690e57630bc26
tehbeard <beardyjames@live.co.uk>
2012-02-08 20:05:28 +0000

    tab to space

--- src/main/java/me/tehbeard/BeardAch/dataSource/configurable/AddonLoader.java
+++ src/main/java/me/tehbeard/BeardAch/dataSource/configurable/AddonLoader.java
@@ -22,86 +22,86 @@ import me.tehbeard.BeardAch.achievement.triggers.ITrigger;
  */
 public class AddonLoader {
 
-	private File dir;
-	private BeardAch plugin;
-	private URLClassLoader loader;
-	public AddonLoader(File dir, BeardAch plugin) {
-		this.dir = dir;
-		this.plugin = plugin;
-		if (!dir.isDirectory()) {
-			throw new IllegalStateException("dir must be a directory!");
-		}
-	}
+    private File dir;
+    private BeardAch plugin;
+    private URLClassLoader loader;
+    public AddonLoader(File dir, BeardAch plugin) {
+        this.dir = dir;
+        this.plugin = plugin;
+        if (!dir.isDirectory()) {
+            throw new IllegalStateException("dir must be a directory!");
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	public void loadAddons() {
-		// get list of files
-		String[] flist = dir.list(new FilenameFilter() {
+    @SuppressWarnings("unchecked")
+    public void loadAddons() {
+        // get list of files
+        String[] flist = dir.list(new FilenameFilter() {
 
-			public boolean accept(File file, String filename) {
-				return !(file.isFile() && filename.endsWith(".jar"));
-			}
-		});
-		HashSet<String> classList = new HashSet<String>();
-		ZipFile addon;
-		URL[] urls = new URL[flist.length];
-		int i =0;
-		BeardAch.printCon(":: "+flist.length);
-		for (String file : flist) {
-			try{
-				File addonFile = new File(dir, file);
+            public boolean accept(File file, String filename) {
+                return !(file.isFile() && filename.endsWith(".jar"));
+            }
+        });
+        HashSet<String> classList = new HashSet<String>();
+        ZipFile addon;
+        URL[] urls = new URL[flist.length];
+        int i =0;
+        BeardAch.printCon(":: "+flist.length);
+        for (String file : flist) {
+            try{
+                File addonFile = new File(dir, file);
 
-				urls[i] = addonFile.toURI().toURL();
-				i++;
-				BeardAch.printCon(addonFile.toURI().toURL().toString());
+                urls[i] = addonFile.toURI().toURL();
+                i++;
+                BeardAch.printCon(addonFile.toURI().toURL().toString());
 
-				addon = new ZipFile(addonFile);
-				ZipEntry manifest = addon.getEntry("achaddon.yml");
-				if (manifest != null) {
-					YamlConfiguration addonConfig = new YamlConfiguration();
-					addonConfig.load(addon.getInputStream(manifest));
-					BeardAch.printCon("Loading addon " + addonConfig.getString("name","N/A"));
-					for(String className:addonConfig.getStringList("classes")){
-						classList.add(className);
-					}
-				}	
-			} catch (ZipException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InvalidConfigurationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-
-		}
-		loader = new URLClassLoader(urls,getClass().getClassLoader());
-		// add to triggers/rewards
-		try {
+                addon = new ZipFile(addonFile);
+                ZipEntry manifest = addon.getEntry("achaddon.yml");
+                if (manifest != null) {
+                    YamlConfiguration addonConfig = new YamlConfiguration();
+                    addonConfig.load(addon.getInputStream(manifest));
+                    BeardAch.printCon("Loading addon " + addonConfig.getString("name","N/A"));
+                    for(String className:addonConfig.getStringList("classes")){
+                        classList.add(className);
+                    }
+                }	
+            } catch (ZipException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (InvalidConfigurationException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 
 
-			for(String t :classList){
-
-				Class<?> c = loader.loadClass(t);
-				if(c!=null){
-					if(ITrigger.class.isAssignableFrom(c)){
-						plugin.addTrigger((Class<? extends ITrigger>) c);
-					}else if(IReward.class.isAssignableFrom(c)){
-						plugin.addReward((Class<? extends IReward>) c);
-					}
-				}
-
-			}
+        }
+        loader = new URLClassLoader(urls,getClass().getClassLoader());
+        // add to triggers/rewards
+        try {
 
 
+            for(String t :classList){
 
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+                Class<?> c = loader.loadClass(t);
+                if(c!=null){
+                    if(ITrigger.class.isAssignableFrom(c)){
+                        plugin.addTrigger((Class<? extends ITrigger>) c);
+                    }else if(IReward.class.isAssignableFrom(c)){
+                        plugin.addReward((Class<? extends IReward>) c);
+                    }
+                }
 
-	}
+            }
+
+
+
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
 }
