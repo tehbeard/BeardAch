eef3e856a1b199d3cd50ffa55ec672385d568ebd
James <beardyjames@live.co.uk>
2012-08-10 02:16:39 +0100

    Add King of the hill cuboid trigger

---
+++ src/main/java/me/tehbeard/BeardAch/achievement/triggers/CuboidKingOfTheHillTrigger.java
@@ -0,0 +1,84 @@
+package me.tehbeard.BeardAch.achievement.triggers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import me.tehbeard.BeardAch.dataSource.configurable.Configurable;
+import me.tehbeard.utils.cuboid.Cuboid;
+
+
+import org.bukkit.entity.Player;
+
+/**
+ * Checks if a players is in a cuboid for a specified amount of time
+ * @author James
+ *
+ */
+@Configurable(tag="koth")
+public class CuboidKingOfTheHillTrigger implements ITrigger, Runnable {
+
+
+    private Cuboid c = new Cuboid();
+    private int time = 0;
+
+    private Map<String,Long> times = new HashMap<String, Long>();
+    
+    
+    public Cuboid getCuboid(){
+        return c;
+    }
+
+    public void configure(String config) {
+        String[] con= config.split("\\/");
+        if(con.length == 2){
+            c.setCuboid(con[0]);
+            time = Integer.parseInt(con[1]);
+        }
+    }
+
+    public boolean checkAchievement(Player player) {
+
+        boolean wasInside = wasInside(player.getName());
+        boolean isInside  = c.isInside(player.getLocation());
+        long currentTime = System.currentTimeMillis() / 1000L;
+        
+        if(wasInside){
+            if((currentTime-getTime(player.getName()))>=time){
+                times.remove(player.getName());
+                return true;
+            }
+            if(!isInside){
+                times.remove(player.getName());
+            }
+        }
+        if(isInside && !wasInside){
+            getTime(player.getName());
+        }
+        
+        return false;
+    }
+
+    public ArrayList<String> getCache(){
+        return c.getChunks();
+    }
+    
+    private boolean wasInside(String player){
+        return times.containsKey(player);
+    }
+    
+    private Long getTime(String player){
+        if(!wasInside(player)){
+            times.put(player, System.currentTimeMillis()/1000L);
+        }
+        return times.get(player);
+    }
+
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+
+}
