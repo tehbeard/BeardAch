add387d1a6e2e3396058f80fc6292ee2b25c3898
James <beardyjames@live.co.uk>
2012-08-26 19:09:24 +0100

    derped. added Listener import.

--- src/main/java/me/tehbeard/BeardAch/achievement/triggers/WorldGuardRegionTrigger.java
+++ src/main/java/me/tehbeard/BeardAch/achievement/triggers/WorldGuardRegionTrigger.java
@@ -8,6 +8,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
