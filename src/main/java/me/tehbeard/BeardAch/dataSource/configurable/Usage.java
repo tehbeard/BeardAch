8692752f2b3678a6de32c7cba60cd4537cdf8e19
James <beardyjames@live.co.uk>
2012-10-03 12:58:43 +0100

    Add usage annotation

---
+++ src/main/java/me/tehbeard/BeardAch/dataSource/configurable/Usage.java
@@ -0,0 +1,11 @@
+package me.tehbeard.BeardAch.dataSource.configurable;
+
+import java.lang.annotation.RetentionPolicy;
+
+import java.lang.annotation.Retention;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Usage {
+String[] arguments();
+String[] dependencies() default {};
+}
