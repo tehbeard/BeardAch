73977f5e392d494a2962c7a240563a8486680b7a
James <beardyjames@live.co.uk>
2012-12-02 00:15:43 +0000

    Update Addon loader to use new bundle format

--- src/main/java/me/tehbeard/BeardAch/achievement/BeardAchAddonLoader.java
+++ src/main/java/me/tehbeard/BeardAch/achievement/BeardAchAddonLoader.java
@@ -4,11 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.YamlConfiguration;
 
 import me.tehbeard.BeardAch.BeardAch;
 import me.tehbeard.BeardAch.achievement.rewards.IReward;
@@ -18,10 +16,10 @@ import me.tehbeard.BeardAch.dataSource.configurable.IConfigurable;
 import me.tehbeard.utils.addons.AddonLoader;
 
 public class BeardAchAddonLoader extends  AddonLoader<IConfigurable> {
-    
+
     public int addonTriggersMetric = 0;
     public int addonRewardsMetric = 0;
-    
+
     public BeardAchAddonLoader(File dir) {
         super(dir, IConfigurable.class);
     }
@@ -30,24 +28,28 @@ public class BeardAchAddonLoader extends  AddonLoader<IConfigurable> {
     public List<String> getClassList(ZipFile addon) {
         List<String> classList = new ArrayList<String>();
         try {
-            ZipEntry manifest = addon.getEntry("achaddon.yml");
-            if (manifest != null) {
-                YamlConfiguration addonConfig = new YamlConfiguration();
+            ZipEntry manifest = addon.getEntry("bundle.properties");
 
-                addonConfig.load(addon.getInputStream(manifest));
+            if(manifest!=null){
+                BeardAch.printCon("Loading addon");
 
-                BeardAch.printCon("Loading addon " + addonConfig.getString("name","N/A"));
-                for(String className:addonConfig.getStringList("classes")){
-                    classList.add(className);
+                Scanner scanner;
+
+                scanner = new Scanner(addon.getInputStream(manifest));
+                while(scanner.hasNext()){
+                    String ln = scanner.nextLine();
+                    String[] l = ln.split("=");
+                    if(l[0].equalsIgnoreCase("name")){
+                        BeardAch.printCon("Loading addon " + l[1]);
+                    }else if(l[0].equalsIgnoreCase("class")){
+                        classList.add(l[1]);
+                    }
                 }
-            }
+                scanner.close();
+            }   
+
         } catch (IOException e) {
             BeardAch.printError("An I/O error occured while trying to access an addon. " + addon.getName());
-            if(BeardAch.self.getConfig().getBoolean("general.debug")){
-                e.printStackTrace();
-            }
-        } catch (InvalidConfigurationException e) {
-            BeardAch.printError("Configuration header for "+ addon.getName() + " appears to be corrupt");
             if(BeardAch.self.getConfig().getBoolean("general.debug")){
                 e.printStackTrace();
             }
