0e1e7cf689a113cb532768a16108753614be3029
James <beardyjames@live.co.uk>
2013-05-26 14:34:00 +0100

    Send achievement description to player who unlocked it when broadcasting globally

--- src/main/java/me/tehbeard/BeardAch/achievement/Achievement.java
+++ src/main/java/me/tehbeard/BeardAch/achievement/Achievement.java
@@ -157,6 +157,7 @@ public class Achievement {
                                     )
                     )
                     );
+            player.sendMessage(ChatColor.BLUE + descrip);
 
         }break;
           
