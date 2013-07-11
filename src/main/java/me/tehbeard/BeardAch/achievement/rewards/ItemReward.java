6a3e75244f6c173c7e6c8aaa23c367542d8b73b6
James <beardyjames@live.co.uk>
2013-07-11 21:35:18 +0100

    Add ItemReward

---
+++ src/main/java/me/tehbeard/BeardAch/achievement/rewards/ItemReward.java
@@ -0,0 +1,54 @@
+package me.tehbeard.BeardAch.achievement.rewards;
+
+import java.util.HashMap;
+
+import me.tehbeard.BeardAch.achievement.Achievement;
+import me.tehbeard.BeardAch.dataSource.configurable.Configurable;
+import me.tehbeard.BeardAch.dataSource.json.editor.EditorField;
+import me.tehbeard.BeardAch.dataSource.json.editor.EditorFieldType;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import com.google.gson.annotations.Expose;
+import com.tehbeard.utils.misc.ItemSyntax;
+
+@Configurable(name="Item reward",tag="item")
+public class ItemReward implements IReward {
+    
+    @Expose
+    @EditorField(alias="Item String (CraftBook syntax)",type=EditorFieldType.text)
+    private String itemStr;
+    
+    @Expose
+    @EditorField(alias="try placed in enderchest",type=EditorFieldType.bool)
+    private boolean tryEnderChest = true;
+    
+    @Expose
+    @EditorField(alias="drop item in world",type=EditorFieldType.bool)
+    private boolean tryDrop = true;
+    
+    private ItemStack item;
+
+    @Override
+    @Deprecated
+    public void configure(Achievement ach, String config) {
+    }
+
+    @Override
+    public void configure(Achievement ach) {
+       item = ItemSyntax.getItem(itemStr);
+    }
+
+    @Override
+    public void giveReward(Player player) {
+        if(player.getInventory().addItem(item).size() == 0){return;}
+        
+        if(tryEnderChest && player.getEnderChest().addItem(item).size() == 0){return;}
+        
+        if(tryDrop){
+            player.getWorld().dropItemNaturally(player.getLocation(), item);
+        }
+    }
+
+}
