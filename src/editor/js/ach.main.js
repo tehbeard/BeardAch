9bfa56a95b7521110e520a3f3ca6a22294d26ab7
James <beardyjames@live.co.uk>
2013-09-23 19:22:26 +0100

    new Achievement editor 90% done.

--- src/editor/js/ach.main.js
+++ src/editor/js/ach.main.js
@@ -1,6 +1,100 @@
-angular.module('achMain',['achDirectives']).controller('achList',function($scope){
+angular.module('achMain',['achDirectives']).controller('achList',function($scope,$templateCache,$filter){
   console.log("Loading achievement list");
-  $scope.load = function(){
-    alert("BORK BORK BORK");
+  $scope.achievements = [];
+
+  $scope.ui = {};
+  $scope.ui.deleteIndex = -1;
+  $scope.ui.editIndex   = -1;
+  $scope.ui.pageStart   = 1;
+  $scope.ui.perPage   = 10;
+  $scope.ui.search      = "";
+
+  $scope.ui.pages = function(){
+  	return Math.ceil($filter('achsearch')($scope.achievements,$scope.ui.search).length / $scope.ui.perPage);
   }
+
+  $scope.ui.inc = function(){
+    $scope.ui.pageStart ++;
+
+    max = $scope.ui.pages();
+    if($scope.ui.pageStart >= max){
+    	$scope.ui.pageStart = max;
+    }
+  }
+
+  $scope.ui.dec = function(){
+    $scope.ui.pageStart --;
+    if($scope.ui.pageStart < 1){
+    	$scope.ui.pageStart = 1;
+    }
+  }
+
+  $scope.newAch = function(){
+  	$scope.achievements.unshift(JSON.parse($templateCache.get('newAchJSON')));
+  	$scope.achievements[0].slug = "" + $scope.achievements.length;
+  }
+
+  /*
+  Show the remove modal
+  */
+  $scope.remove = function(idx){
+  	$scope.ui.deleteIndex = idx;
+  	$("#modalConfirmDelete").modal('show');
+  }
+
+  $scope.edit = function(idx){
+  	$scope.ui.editIndex = idx;
+  	$("#modalEdit").modal('show');
+  }
+
+  $scope.load = function(){
+  	$("#modalLoad").modal('show');
+  }
+
+  $scope.save = function(){
+  	$('#modalSave').modal('show');
+  }
+  $scope.saveAsFile = function(){
+  	window.saveAs(new Blob([angular.toJson($scope.achievements,"  ")]),"ach.json");
+  }
+
+  /*
+  Actually delete from the model.
+  */
+  $scope._actualDelete = function(){
+  	if($scope.ui.deleteIndex != -1){
+  	  $scope.achievements.splice($scope.ui.deleteIndex,1);
+  	  $scope.ui.deleteIndex = -1;
+  	  $("#modalConfirmDelete").modal('hide');
+    }
+  }
+
+  $scope.loadFromFile = function(selector){
+    fr = new FileReader();
+    fr.onload = function(){
+      $scope.achievements = JSON.parse(fr.result);
+       $scope.$apply();
+      $("#modalLoad").modal('hide');
+    }
+    fr.readAsText($(selector)[0].files[0]);
+  }
+})
+.filter('startat',function(){
+	return function(input,start){
+		na = [];
+		for(x = start;x<input.length;x++){
+			na.push(input[x]);
+		}
+		return na;
+	}
+}).filter('achsearch',function(){
+	return function(input,key){
+		na = [];
+		for(x = 0;x<input.length;x++){
+		  if(input[x].slug.indexOf(key) != -1 || input[x].name.indexOf(key) != -1 ){
+			na.push(input[x]);
+		  }
+		}
+		return na;
+	}
 });
