65b1e4855cba1bf41f8a702dae7b8ba24b76cfa6
James <beardyjames@live.co.uk>
2013-10-06 13:33:55 +0100

    begin rework to make trigger lists reusable.

--- src/editor/js/ach.directives.js
+++ src/editor/js/ach.directives.js
@@ -31,4 +31,30 @@ angular.module('achDirectives',[]).directive('achReward', ['$compile','$template
     },
     link: linker
   };
+}])
+.directive('achTriggerList', ['$compile','$templateCache', function($compile,$templateCache) {
+  var linker = function(scope, element, attrs) {
+
+    var html = $templateCache.get('partial.ele.trigger');
+    element.html(html);
+    element.replaceWith($compile(element.html())(scope));
+
+  }
+
+  return {
+    restrict: 'E',
+    scope: {
+        list:'=',
+        
+    },
+    link: linker,
+    controller: function($scope){
+      selected = -1;
+      $scope.ui = {
+        addTrigger: function(){$scope.list.push({_type:$scope.ui.selected});},
+        remItem: function(list,index){if(confirm("Really delete this item?")){list.splice(idx,1);}},
+      }
+  }
+    }
+  };
 }]);
