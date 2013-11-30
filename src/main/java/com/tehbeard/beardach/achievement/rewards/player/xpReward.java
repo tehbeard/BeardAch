7df76e045d095d8aa31e45c9181cb359e0bc2d2f
James <beardyjames@live.co.uk>
2013-11-30 00:26:54 +0000

    Initial test of numeric EditorField, need way to set min/max values.

--- src/main/java/com/tehbeard/beardach/achievement/rewards/player/xpReward.java
+++ src/main/java/com/tehbeard/beardach/achievement/rewards/player/xpReward.java
@@ -7,19 +7,34 @@ import com.tehbeard.beardach.achievement.Achievement;
 import com.tehbeard.beardach.achievement.rewards.IReward;
 import com.tehbeard.beardach.annotations.Configurable;
 import com.tehbeard.beardach.datasource.json.editor.EditorField;
+import com.tehbeard.beardach.datasource.json.editor.EditorFieldType;
 import com.tehbeard.beardach.datasource.json.help.ComponentHelpDescription;
+import com.tehbeard.beardach.datasource.json.help.ComponentValueDescription;
 
 @ComponentHelpDescription(description = "Gives a player some xp")
 @Configurable(tag = "xp", name = "Give xp")
 public class xpReward implements IReward {
 
     @Expose
-    @EditorField(alias = "Amount to give")
+    @EditorField(alias = "Amount to give",type=EditorFieldType.number)
+    @ComponentValueDescription("Raw amount of XP to give a player (for example, a blaze drops 10 xp")
     int xp;
+    
+    @Expose
+    @EditorField(alias = "Amount of levels to add",type=EditorFieldType.number)
+    @ComponentValueDescription("XP levels to add to a player (note: negative values will take away, and cap at 0)")
+    int xpLevels = 0;
 
     @Override
     public void giveReward(Player player) {
         player.giveExp(xp);
+        if(xpLevels != 0){
+          int newXpLevel = player.getLevel() - xpLevels;
+          if(newXpLevel >= 0){
+              player.setLevel(newXpLevel);
+          }
+        }
+        
     }
 
     @Override
